aa6fbb2dff18f5979fc06e57686c98a25a73c8c5
Johannes Raggam <raggam-nl@adm.at>
2011-01-22 19:29:01 +0100

    vocabs in extra module now

---
+++ collective/teaser/vocabularies.py
@@ -0,0 +1,21 @@
+from zope.interface import directlyProvides
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleVocabulary
+from collective.teaser import MsgFact as _
+from plone.app.imaging.utils import getAllowedSizes
+
+def ImportanceVocabulary(context):
+    items =[(_(u"Highest importance"),'1'),
+           (_(u"High importance"),'2'),
+           (_(u"Normal importance"),'3'),
+           (_(u"Low importance"),'4'),
+           ]
+    return SimpleVocabulary.fromItems(items)
+directlyProvides(ImportanceVocabulary, IVocabularyFactory)
+
+def ImageScaleVocabulary(context):
+    allowed_sizes = getAllowedSizes()
+    items = [("%s(%s, %s)" %(key, value[0], value[1]), key)
+        for key,value in allowed_sizes if allowed_sizes]
+    return SimpleVocabulary.fromItems(items)
+directlyProvides(ImageScaleVocabulary, IVocabularyFactory)
