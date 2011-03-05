6910e813edf71bc09ec3bc3a5c90a6d5df4deae9
Johannes Raggam <raggam-nl@adm.at>
2011-03-05 23:50:57 +0100

    teaser caching invalidation without print debug stmts

--- collective/teaser/subscribers.py
+++ collective/teaser/subscribers.py
@@ -3,6 +3,5 @@ from plone.memoize.ram import global_cache
 def cache_invalidate_teaserlist(obj, event):
     """ Invalidate the _teaserlist cache after updating or adding a teaser.
     """
-    print "INVALIDATE"
     global_cache.invalidate(
             'collective.teaser.browser.common.Renderer._teaserlist')
