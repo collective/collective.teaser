37db546aad0a7962573e80f9a32b333af2420416
Johannes Raggam <raggam-nl@adm.at>
2012-01-23 22:10:24 +0100

    remove unused imports

--- collective/teaser/browser/viewlets.py
+++ collective/teaser/browser/viewlets.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 from zope.component import getMultiAdapter
-from Acquisition import (
-    aq_inner,
-    aq_parent,
-)
 from plone.app.layout.viewlets.common import ViewletBase
 from Products.CMFCore.utils import getToolByName
 
@@ -16,7 +12,7 @@ class TeaserPortletsViewlet(ViewletBase):
     @property
     def display(self):
         return True
-    
+
     def update(self):
         if not self.display:
             self.canManagePortlets = False
@@ -24,7 +20,7 @@ class TeaserPortletsViewlet(ViewletBase):
         context_state = getMultiAdapter(
             (self.context, self.request), name=u'plone_context_state')
         self.manageUrl = '%s/%s' % (context_state.view_url(), self.manage_view)
-        # This is the way it's done in plone.app.portlets.manager, so we'll 
+        # This is the way it's done in plone.app.portlets.manager, so we'll
         # do the same
         mt = getToolByName(self.context, 'portal_membership')
         self.canManagePortlets = mt.checkPermission(
